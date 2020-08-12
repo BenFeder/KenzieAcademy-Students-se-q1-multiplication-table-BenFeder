15fd20ad964827a49465c4db4a4dc13357f0c95d
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-08-12 22:02:27 +0000

    deploy: 28fa99555ff964dbc021a9354ba38b89c8d31c16

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 // output the header row
 document.write('<span class="cell header">&times;</span>');
 let i = 0;
+
 while (i <= 10) {
   document.write('<span class="cell header">' + i + "</span>");
   i++;
 }
+
 document.write("<br>");
+
+let j = 0;
+
+while (j <= 10) {
+  document.write('<span class="cell header">' + j + "</span>");
+  j++;
+
+  let k = 0;
+  while (k <= 10) {
+    document.write('<span class="cell">' + k * j + "</span>");
+    k++;
+  }
+
+  document.write("<br>");
+}
 
 // TODO: Write two nested while loops to output the rest of the multiplication table
